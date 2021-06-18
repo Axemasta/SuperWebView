414faf0c7f06252e69c6b7f93663c97aeb4fbf7e
Alex <33064621+Axemasta@users.noreply.github.com>
2021-06-18 12:37:01 +0100

    Removed chmod from xcode select
    I added this earlier and I think its breaking the update script

--- build/scripts/xcode-update.sh
+++ build/scripts/xcode-update.sh
@@ -5,7 +5,7 @@ xcodeRoot=/Applications/Xcode_$xcodeOverride.app
 
 
 echo '##vso[task.setvariable variable=MD_APPLE_SDK_ROOT;]'$xcodeRoot
-sudo chmod xcode-select --switch $xcodeRoot/Contents/Developer
+sudo xcode-select --switch $xcodeRoot/Contents/Developer
 
 # Apply temporary workaround for https://github.com/actions/virtual-environments/issues/1932
 rm -f ${HOME}/Library/Preferences/Xamarin/Settings.plist
