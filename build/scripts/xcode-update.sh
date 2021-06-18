6ee9012eb3be873e0791196749a1263f4ee4d0d9
Alex <33064621+Axemasta@users.noreply.github.com>
2021-06-18 11:18:57 +0100

    Uncommented xcode update script
    The chmod is now allowing this shell script to execute, lets go!

--- build/scripts/xcode-update.sh
+++ build/scripts/xcode-update.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 # https://github.com/PrismLibrary/Prism/blob/master/build/scripts/xcode-update.sh
-# echo "Setting Xcode Override to: $xcodeRoot"
-# xcodeRoot=/Applications/Xcode_$xcodeOverride.app
+echo "Setting Xcode Override to: $xcodeRoot"
+xcodeRoot=/Applications/Xcode_$xcodeOverride.app
 
 
-# echo '##vso[task.setvariable variable=MD_APPLE_SDK_ROOT;]'$xcodeRoot
-# sudo chmod xcode-select --switch $xcodeRoot/Contents/Developer
+echo '##vso[task.setvariable variable=MD_APPLE_SDK_ROOT;]'$xcodeRoot
+sudo chmod xcode-select --switch $xcodeRoot/Contents/Developer
 
-# # Apply temporary workaround for https://github.com/actions/virtual-environments/issues/1932
-# rm -f ${HOME}/Library/Preferences/Xamarin/Settings.plist
-
-echo "Running script with permissions!"
+# Apply temporary workaround for https://github.com/actions/virtual-environments/issues/1932
+rm -f ${HOME}/Library/Preferences/Xamarin/Settings.plist
