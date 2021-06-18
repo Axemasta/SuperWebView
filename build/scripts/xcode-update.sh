870f71878d8d9b6659ba8e88586017815735cbf4
Alex <33064621+Axemasta@users.noreply.github.com>
2021-06-18 10:50:07 +0100

    Updated shell script, added chmod

--- build/scripts/xcode-update.sh
+++ build/scripts/xcode-update.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # https://github.com/PrismLibrary/Prism/blob/master/build/scripts/xcode-update.sh
-xcodeRoot=/Applications/Xcode_$xcodeOverride.app
 echo "Setting Xcode Override to: $xcodeRoot"
+xcodeRoot=/Applications/Xcode_$xcodeOverride.app
+
 
 echo '##vso[task.setvariable variable=MD_APPLE_SDK_ROOT;]'$xcodeRoot
-sudo xcode-select --switch $xcodeRoot/Contents/Developer
+sudo chmod xcode-select --switch $xcodeRoot/Contents/Developer
 
 # Apply temporary workaround for https://github.com/actions/virtual-environments/issues/1932
 rm -f ${HOME}/Library/Preferences/Xamarin/Settings.plist
