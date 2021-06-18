ee0a3709de161594624a2877fde63d88c696c180
Alex <33064621+Axemasta@users.noreply.github.com>
2021-06-18 14:22:11 +0100

    Pipeline Cleanup + v0.2

--- build/scripts/xcode-update.sh
+++ build/scripts/xcode-update.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # https://github.com/PrismLibrary/Prism/blob/master/build/scripts/xcode-update.sh
 # echo "Setting Xcode Override to: $xcodeVersion"
-xcodeRoot=/Applications/Xcode.app
+xcodeRoot=/Applications/Xcode_$xcodeVersion.app
 
 echo '##vso[task.setvariable variable=MD_APPLE_SDK_ROOT;]'$xcodeRoot
 sudo xcode-select --switch $xcodeRoot/Contents/Developer
 
-/usr/bin/xcodebuild -version
-
-xcversion installed
+# /usr/bin/xcodebuild -version
+# xcversion installed
 
 # Apply temporary workaround for https://github.com/actions/virtual-environments/issues/1932
 rm -f ${HOME}/Library/Preferences/Xamarin/Settings.plist
